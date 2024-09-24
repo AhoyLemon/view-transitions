99850593dffaaa942ad0ee3cd8ead4724bd3f6ec
Lemon <3413200+AhoyLemon@users.noreply.github.com>
2024-09-24 10:31:53 -0500

    Initial commit

---
+++ js/partials/_vue.js
@@ -0,0 +1,20 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    h1: "It works.",
+    h5: "Whenever you're ready, delete this text and start writing code."
+  },
+
+  methods: {
+
+  },
+
+  computed: {
+
+  },
+
+  mounted: function() {
+
+  }
+
+});
